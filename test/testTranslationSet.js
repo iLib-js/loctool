2cf8fcc386fedd5ff229571e7169eff045c938f2
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2016-08-04 22:15:48 -0700

    Added unit test and jsdocs frameworks
    Modified the build.xml to build the js docs. Added a few tests for
    translation set to prove the unit test framework is working.

--- test/testTranslationSet.js
+++ test/testTranslationSet.js
@@ -4,15 +4,51 @@
  * Copyright © 2016, Healthtap, Inc. All Rights Reserved.
  */
 
+if (!TranslationSet) {
+    var TranslationSet = require("../lib/TranslationSet.js");
+    var ResourceString = require("../lib/ResourceString.js");
+}
 
 module.exports = {
     moduleSetUp: function(test) {
-        test.expect(8);
-        console.log("got here");
+        
+        test.done();
     },
     
     testConstructor: function(test) {
-    	console.log("got here");
-    }
+        test.expect(1);
+
+        var ts = new TranslationSet();
+        test.ok(ts);
+        test.done();
+    },
+    
+    testConstructorRightSourceLocaleDefault: function(test) {
+        test.expect(1);
+
+        var ts = new TranslationSet();
+        
+        test.equal(ts.sourceLocale, "zxx-XX");
+        test.done();
+    },
+
+    testConstructorGetEmpty: function(test) {
+        test.expect(2);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        var r = ts.get("asdf");
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        test.done();
+    },
+
 };
       
