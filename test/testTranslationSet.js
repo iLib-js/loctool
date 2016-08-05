2aef0a81400d17f4ccf40172e511d90e30fdc1d3
Edwin Hoogerbeets <edwin.hoogerbeets@healthtap.com>
2016-08-05 12:47:17 -0700

    TranslationSet unit tests now work

--- test/testTranslationSet.js
+++ test/testTranslationSet.js
@@ -15,7 +15,7 @@ module.exports = {
         test.done();
     },
     
-    testConstructor: function(test) {
+    testTranslationSetConstructor: function(test) {
         test.expect(1);
 
         var ts = new TranslationSet();
@@ -23,7 +23,7 @@ module.exports = {
         test.done();
     },
     
-    testConstructorRightSourceLocaleDefault: function(test) {
+    testTranslationSetRightSourceLocaleDefault: function(test) {
         test.expect(1);
 
         var ts = new TranslationSet();
@@ -32,7 +32,18 @@ module.exports = {
         test.done();
     },
 
-    testConstructorGetEmpty: function(test) {
+    testTranslationSetGetEmpty: function(test) {
+        test.expect(1);
+
+        var ts = new TranslationSet();
+        
+        var r = ts.get("asdf");
+        
+        test.ok(!r);
+        test.done();
+    },
+    
+    testTranslationSetGet: function(test) {
         test.expect(2);
 
         var ts = new TranslationSet();
@@ -50,5 +61,256 @@ module.exports = {
         test.done();
     },
 
+    testTranslationSetGetUndefined: function(test) {
+        test.expect(1);
+
+        var ts = new TranslationSet();
+        var r = ts.get();
+        
+        test.ok(!r);
+        test.done();
+    },
+    
+    testTranslationSetGetFromMany: function(test) {
+        test.expect(4);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        res = new ResourceString({
+            id: "qwerty",
+            source: "This is another test"
+        });
+        
+        ts.add(res);
+        
+        var r = ts.get("asdf");
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        
+        r = ts.get("qwerty");
+        
+        test.equal(r.getId(), "qwerty");
+        test.equal(r.getSource(), "This is another test");
+        test.done();
+    },
+
+    testTranslationSetGetBySource: function(test) {
+        test.expect(2);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        var r = ts.getBySource("This is a test");
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        test.done();
+    },
+
+    testTranslationSetGetBySourceFromMany: function(test) {
+        test.expect(4);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        res = new ResourceString({
+            id: "qwerty",
+            source: "This is another test"
+        });
+        
+        ts.add(res);
+        
+        var r = ts.getBySource("This is a test");
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        
+        r = ts.getBySource("This is another test");
+        
+        test.equal(r.getId(), "qwerty");
+        test.equal(r.getSource(), "This is another test");
+        test.done();
+    },
+
+    testTranslationSetGetBySourceUndefined: function(test) {
+        test.expect(1);
+
+        var ts = new TranslationSet();
+        var r = ts.getBySource();
+        
+        test.ok(!r);
+        test.done();
+    },
+
+    testTranslationSetGetAll: function(test) {
+        test.expect(6);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        res = new ResourceString({
+            id: "qwerty",
+            source: "This is another test"
+        });
+        
+        ts.add(res);
+        
+        var resources = ts.getAll();
+        test.ok(resources);
+        test.equal(resources.length, 2);
+        
+        var r = resources[0];
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        
+        r = resources[1];
+        
+        test.equal(r.getId(), "qwerty");
+        test.equal(r.getSource(), "This is another test");
+        test.done();
+    },
+
+    testTranslationSetGetAllEmpty: function(test) {
+        test.expect(2);
+
+        var ts = new TranslationSet();
+        var r = ts.getAll();
+        test.ok(r);
+        test.equal(r.length, 0);
+        
+        test.done();
+    },
+    
+    testTranslationSetAddTranslationMerged: function(test) {
+        test.expect(2);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        res = new ResourceString({
+            id: "asdf",
+            source: "This is another test",
+            locale: "de-DE"
+        });
+        
+        ts.add(res);
+        
+        var resources = ts.getAll();
+        test.ok(resources);
+        test.equal(resources.length, 1);
+        test.done();
+    },
+
+    testTranslationSetAddTranslationTranslationAvailable: function(test) {
+        test.expect(3);
+
+        var ts = new TranslationSet();
+        var res = new ResourceString({
+            id: "asdf",
+            source: "This is a test"
+        });
+        
+        ts.add(res);
+        
+        res = new ResourceString({
+            id: "asdf",
+            source: "This is another test",
+            locale: "de-DE"
+        });
+        
+        ts.add(res);
+        
+        var r = ts.get("asdf");
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        
+        var t = r.getTranslation("de-DE");
+        
+        test.equal(t, "This is another test");
+        test.done();
+    },
+
+    testTranslationSetAddAll: function(test) {
+        test.expect(2);
+
+        var ts = new TranslationSet();
+        
+        ts.addAll([
+	        new ResourceString({
+	            id: "asdf",
+	            source: "This is a test"
+	        }),
+	        new ResourceString({
+	            id: "qwerty",
+	            source: "This is another test"
+	        })
+	    ]);
+        
+        var r = ts.get("asdf");
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        test.done();
+    },
+
+    testTranslationSetAddAllAllThere: function(test) {
+        test.expect(6);
+
+        var ts = new TranslationSet();
+        
+        ts.addAll([
+	        new ResourceString({
+	            id: "asdf",
+	            source: "This is a test"
+	        }),
+	        new ResourceString({
+	            id: "qwerty",
+	            source: "This is another test"
+	        })
+	    ]);
+        
+        var resources = ts.getAll();
+        test.ok(resources);
+        test.equal(resources.length, 2);
+        
+        var r = resources[0];
+        
+        test.equal(r.getId(), "asdf");
+        test.equal(r.getSource(), "This is a test");
+        
+        r = resources[1];
+        
+        test.equal(r.getId(), "qwerty");
+        test.equal(r.getSource(), "This is another test");
+        test.done();
+    }
 };
-      
