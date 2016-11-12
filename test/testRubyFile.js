94ffcacff1c45ed48cb0ca3ed29b870619e55a0f
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2016-11-12 01:50:17 -0800

    Ruby file handler now works properly.

--- test/testRubyFile.js
+++ test/testRubyFile.js
@@ -26,7 +26,7 @@ module.exports = {
         	sourceLocale: "en-US"
         }, "./testfiles");
         
-        var rf = new RubyFile(p, "./ruby/t1.rb");
+        var rf = new RubyFile(p, "./ruby/external_user_metric.rb");
         
         test.ok(rf);
         
@@ -56,13 +56,13 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        test.equal(rf.makeKey("This is a test"), "r654479252");
+        test.equal(rf.makeKey("This is a test"), "This_is_a_test");
         
         test.done();
     },
 
-    testRubyFileMakeKeySimpleTexts1: function(test) {
-        test.expect(6);
+    testRubyFileMakeKeyCompressUnderscores: function(test) {
+        test.expect(2);
 
         var p = new AndroidProject({
         	sourceLocale: "en-US"
@@ -71,17 +71,13 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
 
-        test.equals(rf.makeKey("Medications in your profile"), "r32020327");
-		test.equals(rf.makeKey("All medications"), "r835310324");
-		test.equals(rf.makeKey("Conditions"), "r103883086");
-		test.equals(rf.makeKey("Symptoms"), "r481086103");
-		test.equals(rf.makeKey("Experts"), "r343852585");
+        test.equals(rf.makeKey("Medications    in $$$  your profile"), "Medications_in_your_profile");
         
         test.done();
 	},
 
-	testRubyFileMakeKeySimpleTexts2: function(test) {
-        test.expect(6);
+	testRubyFileMakeKeyCompressUnderscores2: function(test) {
+        test.expect(2);
 
         var p = new AndroidProject({
         	sourceLocale: "en-US"
@@ -90,91 +86,10 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
 
-        test.equals(rf.makeKey("Procedures"), "r807691021");
-		test.equals(rf.makeKey("Health Apps"), "r941505899");
-		test.equals(rf.makeKey("Conditions in your profile"), "r240633868");
-		test.equals(rf.makeKey("Treatment Reviews"), "r795086964");
-		test.equals(rf.makeKey("Answers"), "r221604632");
+        test.equals(rf.makeKey("Terms___and___Conditions"), "Terms_and_Conditions");
         
         test.done();
 	},
-
-	testRubyFileMakeKeySimpleTexts3: function(test) {
-        test.expect(11);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-
-        test.equals(rf.makeKey("Private Health Profile"), "r669315500");
-		test.equals(rf.makeKey("People you care for"), "r710774033");
-		test.equals(rf.makeKey("Notifications"), "r284964820");
-		test.equals(rf.makeKey("News"), "r613036745");
-		test.equals(rf.makeKey("More Tips"), "r216617786");
-		test.equals(rf.makeKey("Goals"), "r788359072");
-		test.equals(rf.makeKey("Referral Link"), "r140625167");
-		test.equals(rf.makeKey("Questions"), "r256277957");
-		test.equals(rf.makeKey("Private consults"), "r18128760");
-		test.equals(rf.makeKey("Suggested doctors for you"), "r584966709");
-        
-        test.done();
-	},
-
-	testRubyFileMakeKeyEscapes: function(test) {
-        test.expect(3);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-
-        test.equals(rf.makeKey("Can\'t find treatment id"), "r926831062");
-		test.equals(rf.makeKey("Can\'t find an application for SMS"), "r909283218");
-        
-        test.done();
-	},
-	
-	testRubyFileMakeKeyPunctuation: function(test) {
-        test.expect(8);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-
-        test.equals(rf.makeKey("{topic_name}({topic_generic_name})"), "r382554039");
-		test.equals(rf.makeKey("{doctor_name}, {sharer_name} {start}found this helpful{end}"), "r436261634");
-		test.equals(rf.makeKey("{sharer_name} {start}found this helpful{end}"), "r858107784");
-		test.equals(rf.makeKey("Grow your Care-Team"), "r522565682");
-		test.equals(rf.makeKey("Failed to send connection request!"), "r1015770123");
-		test.equals(rf.makeKey("{goal_name} Goals"), "r993422001");
-		test.equals(rf.makeKey("Referral link copied!"), "r201354363");
-        
-        test.done();
-	},
-
-    testRubyFileMakeKeySameStringMeansSameKey: function(test) {
-        test.expect(3);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-        
-        test.equal(rf.makeKey("This is a test"), "r654479252");
-        test.equal(rf.makeKey("This is a test"), "r654479252");
-        
-        test.done();
-    },
 
     testRubyFileParseSimpleGetByKey: function(test) {
         test.expect(5);
@@ -186,16 +101,16 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test")');
+        rf.parse('Rb.t("This is a test")');
         
         var set = rf.getTranslationSet();
         test.ok(set);
         
-        var r = set.get("r654479252");
+        var r = set.get("This_is_a_test");
         test.ok(r);
         
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         
         test.done();
     },
@@ -210,7 +125,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test")');
+        rf.parse('Rb.t("This is a test")');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -218,7 +133,7 @@ module.exports = {
         var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         
         test.done();
     },
@@ -233,7 +148,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("")');
+        rf.parse('Rb.t("")');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -253,7 +168,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('   RB.getString  (    \t "This is a test"    );  ');
+        rf.parse('   Rb.t  (    \t "This is a test"    );  ');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -261,7 +176,7 @@ module.exports = {
         var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         
         test.done();
     },
@@ -280,7 +195,7 @@ module.exports = {
         var set = rf.getTranslationSet();
         test.equal(set.size(), 0);
 
-        rf.parse('RB.getString("This is a test")');
+        rf.parse('Rb.t("This is a test")');
         
         test.ok(set);
         
@@ -299,7 +214,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('\tRB.getString("This is a test"); // i18n: this is a translator\'s comment\n\tfoo("This is not");');
+        rf.parse('\tRb.t("This is a test"); # i18n: this is a translator\'s comment\n\tfoo("This is not");');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -307,76 +222,8 @@ module.exports = {
         var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         test.equal(r.getComment(), "this is a translator's comment");
-        
-        test.done();
-    },
-
-    testRubyFileParseSimpleWithUniqueIdAndTranslatorComment: function(test) {
-        test.expect(6);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-        
-        rf.parse('\tRB.getString("This is a test", "foobar"); // i18n: this is a translator\'s comment\n\tfoo("This is not");');
-        
-        var set = rf.getTranslationSet();
-        test.ok(set);
-        
-        var r = set.get("foobar");
-        test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "foobar");
-        test.equal(r.getComment(), "this is a translator's comment");
-        
-        test.done();
-    },
-
-    testRubyFileParseWithKey: function(test) {
-        test.expect(5);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-        
-        rf.parse('RB.getString("This is a test", "unique_id")');
-        
-        var set = rf.getTranslationSet();
-        test.ok(set);
-        
-        var r = set.get("unique_id");
-        test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "unique_id");
-        
-        test.done();
-    },
-
-    testRubyFileParseWithKeyCantGetBySource: function(test) {
-        test.expect(3);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-        
-        rf.parse('RB.getString("This is a test", "unique_id")');
-        
-        var set = rf.getTranslationSet();
-        test.ok(set);
-        
-        var r = set.getBySource("This is a test");
-        test.ok(!r);
         
         test.done();
     },
@@ -391,7 +238,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test");\n\ta.parse("This is another test.");\n\t\tRB.getString("This is also a test");');
+        rf.parse('Rb.t("This is a test");\n\ta.parse("This is another test.");\n\t\tRb.t("This is also a test");');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -399,42 +246,12 @@ module.exports = {
         var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         
         r = set.getBySource("This is also a test");
         test.ok(r);
         test.equal(r.getSource(), "This is also a test");
-        test.equal(r.getKey(), "r999080996");
-        
-        test.done();
-    },
-
-    testRubyFileParseMultipleWithKey: function(test) {
-        test.expect(10);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-        
-        rf.parse('RB.getString("This is a test", "x");\n\ta.parse("This is another test.");\n\t\tRB.getString("This is a test", "y");');
-        
-        var set = rf.getTranslationSet();
-        test.ok(set);
-        
-        var r = set.get("x");
-        test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.ok(!r.getAutoKey());
-        test.equal(r.getKey(), "x");
-        
-        r = set.get("y");
-        test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.ok(!r.getAutoKey());
-        test.equal(r.getKey(), "y");
+        test.equal(r.getKey(), "This_is_also_a_test");
         
         test.done();
     },
@@ -449,7 +266,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test");   // i18n: foo\n\ta.parse("This is another test.");\n\t\tRB.getString("This is also a test");\t// i18n: bar');
+        rf.parse('Rb.t("This is a test");   # i18n: foo\n\ta.parse("This is another test.");\n\t\tRb.t("This is also a test");\t# i18n: bar');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -457,19 +274,19 @@ module.exports = {
         var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         test.equal(r.getComment(), "foo");
         
         r = set.getBySource("This is also a test");
         test.ok(r);
         test.equal(r.getSource(), "This is also a test");
-        test.equal(r.getKey(), "r999080996");
+        test.equal(r.getKey(), "This_is_also_a_test");
         test.equal(r.getComment(), "bar");
         
         test.done();
     },
 
-    testRubyFileParseMultipleWithUniqueIdsAndComments: function(test) {
+    testRubyFileParseMultipleWithParametersAndComments: function(test) {
         test.expect(10);
 
         var p = new AndroidProject({
@@ -479,21 +296,21 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test", "asdf");   // i18n: foo\n\ta.parse("This is another test.");\n\t\tRB.getString("This is also a test", "kdkdkd");\t// i18n: bar');
+        rf.parse('Rb.t("This is a test", "asdf");   # i18n: foo\n\ta.parse("This is another test.");\n\t\tRb.t("This is also a test", "kdkdkd");\t# i18n: bar');
         
         var set = rf.getTranslationSet();
         test.ok(set);
         
-        var r = set.get("asdf");
+        var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "asdf");
+        test.equal(r.getKey(), "This_is_a_test");
         test.equal(r.getComment(), "foo");
         
-        r = set.get("kdkdkd");
+        r = set.getBySource("This is also a test");
         test.ok(r);
         test.equal(r.getSource(), "This is also a test");
-        test.equal(r.getKey(), "kdkdkd");
+        test.equal(r.getKey(), "This_is_also_a_test");
         test.equal(r.getComment(), "bar");
         
         test.done();
@@ -509,7 +326,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test");\n\ta.parse("This is another test.");\n\t\tRB.getString("This is a test");');
+        rf.parse('Rb.t("This is a test");\n\ta.parse("This is another test.");\n\t\tRb.t("This is a test");');
         
         var set = rf.getTranslationSet();
         test.ok(set);
@@ -517,37 +334,9 @@ module.exports = {
         var r = set.getBySource("This is a test");
         test.ok(r);
         test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
+        test.equal(r.getKey(), "This_is_a_test");
         
         test.equal(set.size(), 1);
-        
-        test.done();
-    },
-
-    testRubyFileParseDupsDifferingByKeyOnly: function(test) {
-        test.expect(8);
-
-        var p = new AndroidProject({
-        	sourceLocale: "en-US"
-        }, "./testfiles");
-        
-        var rf = new RubyFile(p);
-        test.ok(rf);
-        
-        rf.parse('RB.getString("This is a test");\n\ta.parse("This is another test.");\n\t\tRB.getString("This is a test", "unique_id");');
-        
-        var set = rf.getTranslationSet();
-        test.ok(set);
-        
-        var r = set.getBySource("This is a test");
-        test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
-        
-        r = set.get("unique_id");
-        test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "unique_id");
         
         test.done();
     },
@@ -562,7 +351,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test" + " and this isnt");');
+        rf.parse('Rb.t("This is a test" + " and this isnt");');
         
         var set = rf.getTranslationSet();
 
@@ -581,7 +370,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString("This is a test" + foobar);');
+        rf.parse('Rb.t("This is a test" + foobar);');
         
         var set = rf.getTranslationSet();
         test.equal(set.size(), 0);
@@ -599,7 +388,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString(foobar);');
+        rf.parse('Rb.t(foobar);');
         
         var set = rf.getTranslationSet();
         test.equal(set.size(), 0);
@@ -617,7 +406,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('RB.getString();');
+        rf.parse('Rb.t();');
         
         var set = rf.getTranslationSet();
         test.equal(set.size(), 0);
@@ -635,7 +424,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('EPIRB.getString("This is a test");');
+        rf.parse('EPIRb.t("This is a test");');
         
         var set = rf.getTranslationSet();
         test.equal(set.size(), 0);
@@ -653,7 +442,7 @@ module.exports = {
         var rf = new RubyFile(p);
         test.ok(rf);
         
-        rf.parse('App.RB.getString("This is a test");');
+        rf.parse('App.Rb.t("This is a test");');
         
         var set = rf.getTranslationSet();
         test.equal(set.size(), 1);
@@ -668,7 +457,7 @@ module.exports = {
         	sourceLocale: "en-US"
         }, "./testfiles");
         
-        var rf = new RubyFile(p, "./ruby/t1.rb");
+        var rf = new RubyFile(p, "./ruby/external_user_metric.rb");
         test.ok(rf);
         
         // should read the file
@@ -676,17 +465,17 @@ module.exports = {
         
         var set = rf.getTranslationSet();
         
-        test.equal(set.size(), 2);
+        test.equal(set.size(), 13);
         
-        var r = set.getBySource("This is a test");
+        var r = set.getBySource("Noted that person %{person_id} has %{source} installed");
         test.ok(r);
-        test.equal(r.getSource(), "This is a test");
-        test.equal(r.getKey(), "r654479252");
-
-        var r = set.get("id1");
+        test.equal(r.getSource(), "Noted that person %{person_id} has %{source} installed");
+        test.equal(r.getKey(), "Noted_that_person_person_id_has_source_installed");
+        
+        r = set.getBySource("data is missing or empty.");
         test.ok(r);
-        test.equal(r.getSource(), "This is a test with a unique id");
-        test.equal(r.getKey(), "id1");
+        test.equal(r.getSource(), "data is missing or empty.");
+        test.equal(r.getKey(), "data_is_missing_or_empty");
         
         test.done();
     },
