bc170df38d0c445563459449d35e07d86083e039
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2016-12-03 20:06:57 -0800

    Update to latest packages.

--- node_modules/devtool/bin/index.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-require('./spawn')(process.argv.slice(2));
--- node_modules/devtool/lib/browser-globals.js
+++
@@ -1,6 +0,0 @@
-// This file will likely grow as new browser
-// globals cause issues with various Node.js modules
-module.exports = [
-  'sessionStorage', 'localStorage',
-  'location', 'history', 'MutationObserver'
-];
