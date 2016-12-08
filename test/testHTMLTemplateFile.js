7b03d7d81e7962d1f706593c7b6ebb7d40c0ff2a
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2016-12-08 13:51:42 -0800

    Fixed all unit tests.

--- test/testHTMLTemplateFile.js
+++ test/testHTMLTemplateFile.js
@@ -2477,10 +2477,10 @@ module.exports = {
         }));
 
         diff(htf.localizeText(translations, "fr-FR"),
-        		'  <span class="foo" onclick="javascript:var a = \\"foo\\", b = \\"bar\\";">asdf</span>');
+        		'  <span class="foo" onclick="javascript:var a = &quot;foo&quot;, b = \\"bar\\";">asdf</span>');
         
         test.equal(htf.localizeText(translations, "fr-FR"),
-        		'  <span class="foo" onclick="javascript:var a = \\"foo\\", b = \\"bar\\";">asdf</span>');
+        		'  <span class="foo" onclick="javascript:var a = &quot;foo&quot;, b = &quot;bar&quot;;">asdf</span>');
 
         test.done();
     },
