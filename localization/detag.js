6c8282d038540195df4fab7083ee764ee805c8ed
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2017-01-09 21:55:32 -0800

    First stab at detagging properly

--- localization/detag.js
+++ localization/detag.js
@@ -2,14 +2,14 @@ var fs = require("fs");
 var log4js = require("log4js");
 var path = require("path");
 
-var Xliff = require("./lib/Xliff.js");
+var Xliff = require("../lib/Xliff.js");
 var TranslationUnit = Xliff.TranslationUnit;
-var TranslationSet = require("./lib/TranslationSet.js");
+var TranslationSet = require("../lib/TranslationSet.js");
 
-log4js.configure(path.dirname(module.filename) + '/log4js.json');
+log4js.configure(path.dirname(module.filename) + '/../log4js.json');
 
 if (process.argv.length < 2) {
-	console.log("Usage: reformat.js filename");
+	console.log("Usage: detag.js filename");
 	System.exit(2);
 }
 
@@ -20,36 +20,20 @@ var result = new Xliff({pathName: process.argv[2]});
 
 var unit, units = xliff.getTranslationUnits();
 
-var replacements = {
-	"x-xib": /%($\d)?@/g,
-	"x-objective-c": /%($\d)?@/g,
-	"java": /%[sdfx]/g,
-	"ruby": /[#%]{[^}]*}/g
-};
-
 console.log("Processing translation units ...");
+
+var endingTag = /<%.*%><\//;
 
 // go through all the new strings and re-use old translations from different files
 for (var i = 0; i < units.length; i++) {
 	unit = units[i];
-	//if (unit.state === "translated") {
-	//	unit.state = undefined;
-	//}
-	if (unit.state === "new") {
-		unit.state = "translated";
-	}
 	
-	if (!unit.target || !unit.targetLocale) {
-		console.log("Skipping unit with no target... " + unit.key);
-		units.splice(i, 1);
-		i--; // go back and check the i'th unit again now that it is a different one
-/*
-	} else if (tu.datatype) {
-		var re = replacements[tu.datatype];
-		while (re.exec(unit.source)) {
-			
+	if (unit.datatype === "html") {
+		if (endingTag.test(unit.source) && endingTag.test(unit.target)) {
+			unit.source = unit.source.replace(endingTag, "");
+			unit.target = unit.target.replace(endingTag, "");
+			unit.reskey = unit.reskey.replace(endingTag, "");
 		}
-*/
 	}
 }
 
