7f407e974054087da73e7711ce785bd5cf52bf3f
Edwin Hoogerbeets <edwin.hoogerbeets@healthtap.com>
2017-01-17 23:12:41 -0800

    clean up

--- localization/concatenate.sh
+++ localization/concatenate.sh
@@ -7,24 +7,5 @@ ls lockit*/postprocessed/*/${project}*
 
 	echo 
 	cat $(ls lockit*/postprocessed/*/${project}*) | awk -f concatenate.awk > current/${project}.xliff
-	
-# for i in lockit[1-7] lockit5a lockit6a lockit6b
-#	for i in lockit*
-#	do
-#		echo "  $i"
-#		cd $i/postprocessed
-#		
-#		X=$(ls es/${project}* zh/${project}*)
-#		if [ "$X" != "" ]
-#		then
-#			echo "    Concatenating postprocessed files"
-#			cat es/${project}* zh/${project}* | awk -f ../../concatenate.awk > ${project}.xliff
-#			xmllint --noout ${project}.xliff
-#		fi
-#		cd ../..
-#	done
-#
-#	echo "  Concatenating lockits"
-#	cat lockit[1-8]*/postprocessed/${project}.xliff | awk -f concatenate.awk > current/${project}.xliff
 done
 
