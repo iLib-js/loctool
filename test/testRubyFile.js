de1db4634c9ae5e498a6fe3812804fea550eae6b
sidharthm <sidharth.menon92@gmail.com>
2017-01-18 23:12:17 -0800

    ensure out of order works too. JIRA GLOB-1645

--- test/testRubyFile.js
+++ test/testRubyFile.js
@@ -1133,6 +1133,31 @@ module.exports = {
         test.done();
     },
 
+    testRubyFileParseOutOfOrder: function(test) {
+        test.expect(7);
+
+        var p = new WebProject({
+            id: "webapp",
+            sourceLocale: "en-US"
+        }, "./testfiles");
+
+        var rf = new RubyFile({
+            project: p
+        });
+        test.ok(rf);
+
+        rf.parse("Rb.p(:other => \"There are %{count} tests\", one: 'This is 1 test', {count: 1})");
+        var set = rf.getTranslationSet();
+        test.ok(set);
+        test.equal(set.size(), 1);
+        var r = set.getAll()[0];
+        test.ok(r);
+        test.equals(r.get('one'),'This is 1 test');
+        test.equals(r.get('other'),'There are %{count} tests');
+        test.equals(r.getKey(), 'r186608186');
+        test.done();
+    },
+
     testRubyFileParsePluralMissingRequiredKey: function(test) {
         test.expect(3);
 
