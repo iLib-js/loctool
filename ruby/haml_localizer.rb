5ebc2ee6c818143a6593532c8015bf0a3c29b7f4
Edwin Hoogerbeets <edwin.hoogerbeets@healthtap.com>
2017-03-20 12:29:33 -0700

    Update haml localizer to be able to do multiple pseudo-English, and add en-NZ

--- ruby/haml_localizer.rb
+++ ruby/haml_localizer.rb
@@ -8,9 +8,11 @@ require 'json'
 PSEUDO_LOCALE = 'de-DE'
 BRITISH_LOCALE = 'en-GB'
 CANADIAN_LOCALE = 'en-CA'
+NEWZEALAND_LOCALE = 'en-NZ'
 BRITISH_DICTIONARY_PATH = '../db/britishSpellings.json'
 CANADIAN_DICTIONARY_PATH = '../db/canadianSpellings.json'
-SKIP_LOADING_LOCALES = [PSEUDO_LOCALE, BRITISH_LOCALE, CANADIAN_LOCALE]
+NEWZEALAND_DICTIONARY_PATH = '../db/newzealandSpellings.json'
+SKIP_LOADING_LOCALES = [PSEUDO_LOCALE, BRITISH_LOCALE, CANADIAN_LOCALE, NEWZEALAND_LOCALE]
 # Hash that stores start/end mapping of HTML escape sequences
 # Check spec for details
 HTML_ESCAPE_CHARS = {
@@ -212,7 +214,7 @@ end
 
 #param values - the extracted strings that we want to process
 # return <original string> => <string to replace with>
-def process_british_values(values)
+def process_spelling_values(values, spellings)
   ret = {}
   values.each do |v|
     next if v.strip.length == 0
@@ -239,7 +241,7 @@ def process_british_values(values)
         curr_word << c
       end
       if (!is_letter || last_character)
-        processed << check_for_british(curr_word)
+        processed << check_for_correction(curr_word, spellings)
         processed << c unless (last_character && is_letter)
         curr_word = ''
         if HTML_ESCAPE_CHARS.keys.include?(c) and !skipping
@@ -251,87 +253,31 @@ def process_british_values(values)
     ret[v] = processed
   end
   ret
+end
+
+def load_spellings(path)
+  p = __dir__ + "/" + path
+  raise "Dictionary not found at #{p}" unless File.exists?(p)
+  f = File.read(p)
+  puts "Dictionary loaded"
+  JSON.parse(f)
 end
 
 def british_spellings
-  @british_spellings ||= load_british_spellings
-end
-
-def load_british_spellings
-  raise "British dictionary not found at #{BRITISH_DICTIONARY_PATH}" unless File.exists?(BRITISH_DICTIONARY_PATH)
-  f = File.read(BRITISH_DICTIONARY_PATH)
-  puts "British dictionary loaded"
-  JSON.parse(f)
-end
-
-def check_for_british(word)
-  if british_spellings[word.downcase]
-    translated_word = british_spellings[word.downcase]
-    match_case_for_words(translated_word, word)
-  else
-    #add as is to string
-    word
-  end
-end
-
-#param values - the extracted strings that we want to process
-# return <original string> => <string to replace with>
-def process_canadian_values(values)
-  ret = {}
-  values.each do |v|
-    next if v.strip.length == 0
-    processed = ''
-    curr_word = ''
-    as_chars = v.split('')
-    skipping = false
-    matching_chars_to_stop_skipping = []
-    as_chars.each_with_index do |c, i|
-      if skipping
-        # we previously hit an escape character
-        if matching_chars_to_stop_skipping.include?(c)
-          # we hit a stop char, so stop skipping and reset the stop chars
-          skipping = false
-          matching_chars_to_stop_skipping = []
-        end
-        # keep moving forward
-        processed << c
-        next
-      end
-      is_letter = /[[:alpha:]]/.match(c)
-      last_character = i == as_chars.count - 1
-      if is_letter
-        curr_word << c
-      end
-      if (!is_letter || last_character)
-        processed << check_for_canadian(curr_word)
-        processed << c unless (last_character && is_letter)
-        curr_word = ''
-        if HTML_ESCAPE_CHARS.keys.include?(c) and !skipping
-          skipping = true
-          matching_chars_to_stop_skipping = HTML_ESCAPE_CHARS[c]
-        end
-      end
-    end
-    ret[v] = processed
-  end
-  ret
+  @british_spellings ||= load_spellings(BRITISH_DICTIONARY_PATH)
 end
 
 def canadian_spellings
-  @canadian_spellings ||= load_canadian_spellings
+  @canadian_spellings ||= load_spellings(CANADIAN_DICTIONARY_PATH)
 end
 
-def load_canadian_spellings
-  # puts "Canadian dict load in dir #{Dir.pwd}  or  #{__dir__}"
-  p = __dir__ + "/" + CANADIAN_DICTIONARY_PATH
-  raise "Canadian dictionary not found at #{p}" unless File.exists?(p)
-  f = File.read(p)
-  JSON.parse(f)
+def newzealand_spellings
+  @newzealand_spellings ||= load_spellings(NEWZEALAND_DICTIONARY_PATH)
 end
 
-def check_for_canadian(word)
-  if canadian_spellings[word.downcase]
-    translated_word = canadian_spellings[word.downcase]
+def check_for_correction(word, spellings)
+  if spellings[word.downcase]
+    translated_word = spellings[word.downcase]
     match_case_for_words(translated_word, word)
   else
     #add as is to string
@@ -588,9 +534,11 @@ def process_file_content(template, path_name, locale_names, all_locale_mappings)
       from_to = process_pseudo_values(values)
       #unmapped_for_file = values # removing because it makes every string unmapped
     elsif locale_name == BRITISH_LOCALE
-      from_to = process_british_values(values)
+      from_to = process_spelling_values(values, british_spellings)
+    elsif locale_name == NEWZEALAND_LOCALE
+        from_to = process_spelling_values(values, newzealand_spellings)
     elsif locale_name == CANADIAN_LOCALE
-      from_to = process_canadian_values(values)
+      from_to = process_spelling_values(values, canadian_spellings)
     else
       from_to = process_values(locale_mappings, values, unmapped_for_file)
     end
