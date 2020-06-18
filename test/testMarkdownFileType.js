e76a14e39fe398d3920eccda6452db07e3e33964
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2020-06-18 11:53:36 -0700

    Update copyright dates

--- test/testMarkdownFileType.js
+++ test/testMarkdownFileType.js
@@ -1,7 +1,7 @@
 /*
  * testMarkdownFileType.js - test the Markdown file type handler object.
  *
- * Copyright © 2019, Box, Inc.
+ * Copyright © 2019-2020, Box, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
