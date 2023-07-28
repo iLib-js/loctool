b44fb1c3861a5efea2187b0ceadf2e659fd118ca
Goun Lee <goun.lee@lge.com>
2023-07-28 10:24:08 +0900

    update

--- test/testHTMLTemplateFile.js
+++ test/testHTMLTemplateFile.js
@@ -1,7 +1,7 @@
 /*
  * testHTMLTemplateFile.js - test the HTML template file handler object.
  *
- * Copyright © 2016-2017, 2019, 2023HealthTap, Inc.
+ * Copyright © 2016-2017, 2019, 2023 HealthTap, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
