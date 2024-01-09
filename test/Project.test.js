2fcff58fe9c16b26e88c2ab1d4cbc289f25aa341
Goun Lee <goun.lee@lge.com>
2024-01-09 18:13:08 +0900

    Update copyright year

--- test/Project.test.js
+++ test/Project.test.js
@@ -1,7 +1,7 @@
 /*
  * Project.test.js - test Project class
  *
- * Copyright © 2020-2021, 2023 JEDLSoft
+ * Copyright © 2020-2021, 2023-2024 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
